f9998fcb48a61f24615ce437e44c8694e7657d50
rajveer308 <r4jv33rrr@gmail.com>
2024-11-20 13:49:07 +0530

    Updated features and bug fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -58,21 +58,21 @@ const [text, setText] = useState('Enter text here');
   return (
     <>
     <div className="container" style = {{color : props.mode==='dark'?'white' : '#042743'}}>
-        <h1 class = "text-center">{props.heading}</h1>
+        <h1 class = "text-center h2">{props.heading}</h1>
         <div className="mt-3">
-        <textarea className="form-control" value = {text} style = {{backgroundColor : props.mode==='dark'?'grey' : 'white', color : props.mode==='dark'?'white' : '#042743'}} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value = {text} style = {{backgroundColor : props.mode==='dark'?'#13466E' : 'white', color : props.mode==='dark'?'white' : '#042743'}} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mt-3 mx-3" onClick = {handleUpClick} >Convert it to UpperCase</button>
-        <button className="btn btn-primary mt-3 mx-3" onClick = {handleDownClick} >Convert it to LowerCase</button>
-        <button className="btn btn-primary mt-3 mx-3" onClick = {handleinverseclick} >Reverse Text</button>
-        <button className="btn btn-primary mt-3 mx-3" onClick = {handleClearClick} >Clear Text</button>
-        <button className="btn btn-primary mt-3 mx-3" onClick = {handleExtraSpaces} >Remove Extra Spaces</button>
-        <button className="btn btn-primary mt-3 mx-3" onClick = {handleCopy} >Copy Text</button>
-        <button type="submit" onClick={speak} className="btn btn-warning d-grid gap-2 col-6 mx-auto mt-3">Speak</button>
+        <button disabled= {text.length===0} className="btn btn-primary mt-3 mx-3 my-1" onClick = {handleUpClick} >Convert it to UpperCase</button>
+        <button disabled= {text.length===0} className="btn btn-primary mt-3 mx-3 my-1" onClick = {handleDownClick} >Convert it to LowerCase</button>
+        <button disabled= {text.length===0} className="btn btn-primary mt-3 mx-3 my-1" onClick = {handleinverseclick} >Reverse Text</button>
+        <button disabled= {text.length===0} className="btn btn-primary mt-3 mx-3 my-1" onClick = {handleClearClick} >Clear Text</button>
+        <button disabled= {text.length===0} className="btn btn-primary mt-3 mx-3 my-1" onClick = {handleExtraSpaces} >Remove Extra Spaces</button>
+        <button disabled= {text.length===0} className="btn btn-primary mt-3 mx-3 my-1" onClick = {handleCopy} >Copy Text</button>
+        <button disabled= {text.length===0}  type="submit" onClick={speak} className="btn btn-warning d-grid gap-2 col-6 mx-auto mt-3">Speak</button>
     </div>
     <div className="container my-2" style = {{color : props.mode==='dark'?'white' : '#042743'}}>
         <h2>Your text summary -</h2>
-        <p>Your text has {text.length} characters and {text.split(" ").length-1} words.</p>
+        <p>Your text has {text.length} characters and {text.split(" ").filter((element)=> {return element !== 0}).length} words.</p>
         <p>{0.08 * text.split(" ").length} minutes read.</p>
         {/* <h2>Preview -</h2>
         <p>{text.length > 0?text : "Enter something in the textbox above to preview it here"}</p> */}
